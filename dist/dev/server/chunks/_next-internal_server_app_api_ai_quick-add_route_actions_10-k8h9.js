79a6aa974f3332d2d3b701b33b2bf602e26f7e6f
Franco Carrara <frankko212@gmail.com>
2026-05-04 20:49:23 -0300

    chore: remove dist from git tracking

--- dist/dev/server/chunks/_next-internal_server_app_api_ai_quick-add_route_actions_10-k8h9.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/ai/quick-add/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_ai_quick-add_route_actions_10-k8h9.js.map
--- dist/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CzzYAtpIyQSU83cNMEG41MOf6ZTlWgtcsZyaI98yqc0=\"\n}"
