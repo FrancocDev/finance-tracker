0fd9c17aa29d037065fef40e85d1785918f623ce
Franco Carrara <frankko212@gmail.com>
2026-05-07 10:32:41 -0300

    feat: update widget API key

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <Script
           src="https://francocdev-voiceloop.vercel.app/widget.js"
           strategy="afterInteractive"
-          data-api-key="455e7e7160a1eced54e61382dfff639c357df50200527661254396291d0b0728"
+          data-api-key="vl_njuhtSTFUnUBpifJaQ7aMnenyOHHhiVt2LVxWCbWvwI"
           data-site-url="https://francocdev-finance-tracker-ai.vercel.app/"
         />
         {children}
